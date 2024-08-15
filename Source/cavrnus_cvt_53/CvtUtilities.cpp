cca730898fbe1593dc0b7cdcd612617c7b5b04c5
Cody Tedrick <cody.tedrick@cavrn.us>
2024-08-15 08:44:38 -0700

    Cleaned up Xray logic

--- Source/cavrnus_cvt_53/CvtUtilities.cpp
+++ Source/cavrnus_cvt_53/CvtUtilities.cpp
@@ -2,7 +2,9 @@
 
 
 #include "CvtUtilities.h"
-TArray<FTransform> UCvtUtilities::JsonStringToFTransformArray(const FString& JsonStr, const FString& FieldName, bool& Success)
+
+TArray<FTransform> UCvtUtilities::JsonStringToFTransformArray(const FString& JsonStr, const FString& FieldName,
+                                                              bool& Success)
 {
 	TArray<FTransform> Transforms;
 	TSharedPtr<FJsonObject> JsonObject;
@@ -45,7 +47,7 @@ TArray<FString> UCvtUtilities::JsonStringToFStringArray(const FString& JsonStr, 
 	}
 
 	Success = ReturnValue.Num() > 0;
-	
+
 	return ReturnValue;
 }
 
@@ -57,7 +59,7 @@ FTransform UCvtUtilities::JsonObjectToFTransform(TSharedPtr<FJsonObject> JsonObj
 		bool bTranslationSuccess = false;
 		TSharedPtr<FJsonObject> TranslationObject = Data->Values["translation"]->AsObject();
 		const FVector3d Translation = JsonObjectToFVector(TranslationObject, bTranslationSuccess);
-		
+
 		bool bRotationSuccess = false;
 		TSharedPtr<FJsonObject> RotationObject = Data->Values["rotation"]->AsObject();
 		const FQuat Rotation = JsonObjectToFQuat(RotationObject, bRotationSuccess);
@@ -106,4 +108,152 @@ FQuat UCvtUtilities::JsonObjectToFQuat(TSharedPtr<FJsonObject> JsonObject, bool&
 	}
 
 	return FQuat();
+}
+
+FString UCvtUtilities::SerializeSaveGameData(const FCvtSaveGameData& InData, bool& Success)
+{
+	TArray<TSharedPtr<FJsonValue>> ActorNames;
+	for (const FString& Element : InData.ActorNames)
+		ActorNames.Add(MakeShareable(new FJsonValueString(Element)));
+
+	TArray<TSharedPtr<FJsonValue>> ActorTransforms;
+	for (const FTransform& Element : InData.ActorTransforms)
+		ActorTransforms.Add(MakeShareable(new FJsonValueString(Element.ToString())));
+
+	TArray<TSharedPtr<FJsonValue>> XRayNames;
+	for (const FString& Element : InData.XRayNames)
+		XRayNames.Add(MakeShareable(new FJsonValueString(Element)));
+
+	TArray<TSharedPtr<FJsonValue>> DimensionControlLocations;
+	for (const FVector& Element : InData.DimensionControlLocations)
+		DimensionControlLocations.Add(MakeShareable(new FJsonValueString(Element.ToString())));
+
+	TArray<TSharedPtr<FJsonValue>> AnnotationPointTransforms;
+	for (const FTransform& Element : InData.AnnotationPointTransforms)
+		AnnotationPointTransforms.Add(MakeShareable(new FJsonValueString(Element.ToString())));
+
+	TArray<TSharedPtr<FJsonValue>> AnnotationLastIndices;
+	for (const int32& Element : InData.AnnotationLastIndices)
+		AnnotationLastIndices.Add(MakeShareable(new FJsonValueString(FString::FromInt(Element))));
+
+	TArray<TSharedPtr<FJsonValue>> AnnotationColors;
+	for (const FLinearColor& Element : InData.AnnotationColors)
+		AnnotationColors.Add(MakeShareable(new FJsonValueString(Element.ToString())));
+
+	TArray<TSharedPtr<FJsonValue>> AnnotationTexts;
+	for (const FText& Element : InData.AnnotationTexts)
+		AnnotationTexts.Add(MakeShareable(new FJsonValueString(Element.ToString())));
+
+	TArray<TSharedPtr<FJsonValue>> AnnotationTextLocations;
+	for (const FVector& Element : InData.AnnotationTextLocations)
+		AnnotationTextLocations.Add(MakeShareable(new FJsonValueString(Element.ToString())));
+
+	TArray<TSharedPtr<FJsonValue>> AnnotationTargetLocations;
+	for (const FVector& Element : InData.AnnotationTargetLocations)
+		AnnotationTargetLocations.Add(MakeShareable(new FJsonValueString(Element.ToString())));
+
+	const TSharedRef<FJsonObject> JsonObject(new FJsonObject());
+	JsonObject->SetArrayField("ActorNames", ActorNames);
+	JsonObject->SetArrayField("ActorTransforms", ActorTransforms);
+	JsonObject->SetArrayField("XRayNames", XRayNames);
+	JsonObject->SetArrayField("DimensionControlLocations", DimensionControlLocations);
+	JsonObject->SetArrayField("AnnotationPointTransforms", AnnotationPointTransforms);
+	JsonObject->SetArrayField("AnnotationLastIndices", AnnotationLastIndices);
+	JsonObject->SetArrayField("AnnotationColors", AnnotationColors);
+	JsonObject->SetArrayField("AnnotationTexts", AnnotationTexts);
+	JsonObject->SetArrayField("AnnotationTextLocations", AnnotationTextLocations);
+	JsonObject->SetArrayField("AnnotationTargetLocations", AnnotationTargetLocations);
+
+	FString Result;
+	TSharedRef<TJsonWriter<>> JsonWriter = TJsonWriterFactory<>::Create(&Result);
+	FJsonSerializer::Serialize(JsonObject, JsonWriter);
+
+	Success = !Result.IsEmpty();
+
+	return Result;
+}
+
+FCvtSaveGameData UCvtUtilities::DeserializeSaveGameData(const FString& InData, bool& Success)
+{
+	TSharedPtr<FJsonObject> json(new FJsonObject());
+	TSharedRef<TJsonReader<FString::ElementType>> Reader = TJsonReaderFactory<FString::ElementType>::Create(InData);
+	FJsonSerializer::Deserialize(Reader, json);
+
+	FCvtSaveGameData ReturnData;
+
+	TArray<TSharedPtr<FJsonValue>> ActorNames = json->GetArrayField("ActorNames");
+	for (const TSharedPtr<FJsonValue>& Element : ActorNames)
+	{
+		ReturnData.ActorNames.Add(Element->AsString());
+	}
+
+	TArray<TSharedPtr<FJsonValue>> ActorTransforms = json->GetArrayField("ActorTransforms");
+	for (const TSharedPtr<FJsonValue>& Element : ActorTransforms)
+	{
+		FTransform Value;
+		Value.InitFromString(Element->AsString());
+		ReturnData.ActorTransforms.Add(Value);
+	}
+
+	TArray<TSharedPtr<FJsonValue>> XRayNames = json->GetArrayField("XRayNames");
+	for (const TSharedPtr<FJsonValue>& Element : XRayNames)
+	{
+		ReturnData.XRayNames.Add(Element->AsString());
+	}
+
+	TArray<TSharedPtr<FJsonValue>> DimensionControlLocations = json->GetArrayField("DimensionControlLocations");
+	for (const TSharedPtr<FJsonValue>& Element : DimensionControlLocations)
+	{
+		FVector Value;
+		Value.InitFromString(Element->AsString());
+		ReturnData.DimensionControlLocations.Add(Value);
+	}
+
+	TArray<TSharedPtr<FJsonValue>> AnnotaionPointTransforms = json->GetArrayField("AnnotaionPointTransforms");
+	for (const TSharedPtr<FJsonValue>& Element : AnnotaionPointTransforms)
+	{
+		FTransform Value;
+		Value.InitFromString(Element->AsString());
+		ReturnData.AnnotationPointTransforms.Add(Value);
+	}
+
+	TArray<TSharedPtr<FJsonValue>> AnnotationLastIndices = json->GetArrayField("AnnotationLastIndices");
+	for (const TSharedPtr<FJsonValue>& Element : AnnotationLastIndices)
+	{
+		ReturnData.AnnotationLastIndices.Add(FCString::Atoi(*Element->AsString()));
+	}
+
+	TArray<TSharedPtr<FJsonValue>> AnnotationColors = json->GetArrayField("AnnotationColors");
+	for (const TSharedPtr<FJsonValue>& Element : AnnotationColors)
+	{
+		FLinearColor Value;
+		Value.InitFromString(Element->AsString());
+		ReturnData.AnnotationColors.Add(Value);
+	}
+
+	TArray<TSharedPtr<FJsonValue>> AnnotationTexts = json->GetArrayField("AnnotationTexts");
+	for (const TSharedPtr<FJsonValue>& Element : AnnotationTexts)
+	{
+		FText Value;
+		Value.FromString(Element->AsString());
+		ReturnData.AnnotationTexts.Add(Value);
+	}
+
+	TArray<TSharedPtr<FJsonValue>> AnnotationTextLocations = json->GetArrayField("AnnotationTextLocations");
+	for (const TSharedPtr<FJsonValue>& Element : AnnotationTextLocations)
+	{
+		FVector Value;
+		Value.InitFromString(Element->AsString());
+		ReturnData.AnnotationTextLocations.Add(Value);
+	}
+
+	TArray<TSharedPtr<FJsonValue>> AnnotationTargetLocations = json->GetArrayField("AnnotationTargetLocations");
+	for (const TSharedPtr<FJsonValue>& Element : AnnotationTargetLocations)
+	{
+		FVector Value;
+		Value.InitFromString(Element->AsString());
+		ReturnData.AnnotationTargetLocations.Add(Value);
+	}
+
+	return ReturnData;
 }
--- Source/cavrnus_cvt_53/CvtUtilities.h
+++ Source/cavrnus_cvt_53/CvtUtilities.h
@@ -5,6 +5,39 @@
 #include "CoreMinimal.h"
 #include "CvtUtilities.generated.h"
 
+USTRUCT(BlueprintType)
+struct FCvtSaveGameData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite)
+	FString SlotName;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Transform Command")
+	TArray<FString> ActorNames;
+	UPROPERTY(BlueprintReadWrite, Category = "Transform Command")
+	TArray<FTransform> ActorTransforms;
+
+	UPROPERTY(BlueprintReadWrite, Category = "XRay Command")
+	TArray<FString> XRayNames;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Dimension Command")
+	TArray<FVector> DimensionControlLocations;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Annotaion Command")
+	TArray<FTransform> AnnotationPointTransforms;
+	UPROPERTY(BlueprintReadWrite, Category = "Annotaion Command")
+	TArray<int32> AnnotationLastIndices;
+	UPROPERTY(BlueprintReadWrite, Category = "Annotaion Command")
+	TArray<FLinearColor> AnnotationColors;
+	UPROPERTY(BlueprintReadWrite, Category = "Annotaion Command")
+	TArray<FText> AnnotationTexts;
+	UPROPERTY(BlueprintReadWrite, Category = "Annotaion Command")
+	TArray<FVector> AnnotationTextLocations;
+	UPROPERTY(BlueprintReadWrite, Category = "Annotaion Command")
+	TArray<FVector> AnnotationTargetLocations;
+};
+
 UCLASS(Abstract)
 class UCvtUtilities : public UBlueprintFunctionLibrary
 {
@@ -14,7 +47,15 @@ public:
 	static FTransform JsonObjectToFTransform(TSharedPtr<FJsonObject> JsonObject, bool& Success);
 	static FVector JsonObjectToFVector(TSharedPtr<FJsonObject> JsonObject, bool& Success);
 	static FQuat JsonObjectToFQuat(TSharedPtr<FJsonObject> JsonObject, bool& Success);
-	
+
+	UFUNCTION(BlueprintCallable, CallInEditor, Exec, Category = "Cavrnus",
+		meta = (ToolTip = "Convert SaveData to Json", ShortToolTip = "Serialize CVT SaveGameData"))
+	static FString SerializeSaveGameData(const FCvtSaveGameData& InData, bool& Success);
+
+	UFUNCTION(BlueprintCallable, CallInEditor, Exec, Category = "Cavrnus",
+		meta = (ToolTip = "Convert Json to SaveGameData", ShortToolTip = "Dsserialize SaveGameData"))
+	static FCvtSaveGameData DeserializeSaveGameData(const FString& InData, bool& Success);
+
 	UFUNCTION(BlueprintCallable, CallInEditor, Exec, Category = "Cavrnus",
 		meta = (ToolTip = "Convert JSON string to an array of FTransforms", ShortToolTip = "Convert JSON string to an array of FTransforms"))
 	static TArray<FTransform> JsonStringToFTransformArray(const FString& JsonStr, const FString& FieldName, bool& Success);
